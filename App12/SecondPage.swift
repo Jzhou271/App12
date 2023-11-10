150d1f9cb0e398d02ae56b3fae1f7a34db0343a1
Jing Zhou <ivy_qq@live.cn>
2023-11-09 19:12:55 -0800

    Create SecondView

---
+++ App12/SecondPage.swift
@@ -0,0 +1,20 @@
+//
+//  SecondPage.swift
+//  App12
+//
+//  Created by Ivy Zhou on 11/9/23.
+//
+
+import UIKit
+
+class SecondPage: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
